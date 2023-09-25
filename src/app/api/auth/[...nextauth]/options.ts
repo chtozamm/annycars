8d962bf0a4ee759ca34a0629749130cc0d5bb2af
Mark M <chtozamm@gmail.com>
2023-09-25 19:27:26 +0300

    authentification

---
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -0,0 +1,31 @@
+import { NextAuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        password: {
+          label: "Пароль",
+          type: "password",
+          placeholder: "",
+        },
+      },
+      async authorize(credentials, req) {
+        // This is where you need to retrieve user data
+        // to verify with credentials
+        const user = {
+          id: "1",
+          password: process.env.AUTH_PASSWORD,
+        };
+
+        if (credentials?.password === user.password) {
+          return user;
+        } else {
+          throw Error("Неверный пароль.");
+        }
+      },
+    }),
+  ],
+};
--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,37 +1,5 @@
-import NextAuth, { NextAuthOptions } from "next-auth";
-import CredentialsProvider from "next-auth/providers/credentials";
-
-export const authOptions: NextAuthOptions = {
-  providers: [
-    CredentialsProvider({
-      name: "Credentials",
-      credentials: {
-        password: {
-          label: "Пароль",
-          type: "password",
-          placeholder: "",
-        },
-      },
-      async authorize(credentials, req) {
-        // This is where you need to retrieve user data
-        // to verify with credentials
-        const user = {
-          id: "1",
-          password: process.env.AUTH_PASSWORD,
-        };
-
-        if (credentials?.password === user.password) {
-          return user;
-        } else {
-          throw Error("Неверный пароль.");
-        }
-      },
-    }),
-  ],
-  pages: {
-    signIn: "/auth/signin",
-  },
-};
+import NextAuth from "next-auth";
+import { authOptions } from "./options";
 
 export const handler = NextAuth(authOptions);
 
